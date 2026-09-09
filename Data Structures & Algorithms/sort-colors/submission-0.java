569faaf1a19d53be5d665e677c12bdeb544fb0fd
Justin See <113479238+justinseeuoit@users.noreply.github.com>
2026-09-09 16:43:15 -0400

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int temp = 0;
+        boolean swapped;
+        for (int i = 0; i < nums.length - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < nums.length - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (swapped == false)
+                break;
+        }
+    }
+}
